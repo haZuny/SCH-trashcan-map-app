fbd53a812980778705d80b351a02ffb36c07ecdb
haZuny <gkwns5791@naver.com>
2022-11-03 11:22:21 +0900

    서버 rest테스트

--- trash_can_map/lib/comu_index.dart
+++ trash_can_map/lib/comu_index.dart
@@ -1,11 +1,26 @@
 import 'package:flutter/material.dart';
 
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class IndexPage extends StatelessWidget{
+  String idF = '';
+  String latF = "";
+  String lonF = "";
+
     @override
     Widget build(BuildContext context){
       return(Scaffold(
-        body: Center(child: Text("곧 구현될 기능입니다.", textAlign: TextAlign.center, style: TextStyle(fontSize: 20),),),
+        body: Container(child: Column(children: [
+          ElevatedButton(onPressed: () async {
+              var res = await http.get(Uri.parse('http://172.30.1.58:8000/bins/5'));
+              var res2 = await http.post(Uri.parse('http://172.30.1.58:8000/bins/'), body: json.encode({'id':'', 'x_location': '100.1234', 'y_location': '100.1234'}));
+              print(json.decode(res.body));
+              print(res2.statusCode);
+              print(json.decode(res2.body));
+
+          }, child: Text("버튼"))
+        ],),),
       ));
     }
 }
